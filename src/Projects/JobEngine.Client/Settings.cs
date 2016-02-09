5310eb325d0618ce1a67d4d2b180e35d78a77d3d
Scott Thornton <sthornton@users.noreply.github.com>
2016-02-09 14:29:49 -0500

    updated seed script, config changes

--- src/Projects/JobEngine.Client/Settings.cs
+++ src/Projects/JobEngine.Client/Settings.cs
@@ -34,13 +34,13 @@ namespace JobEngine.Client
         {
 
 #if DEBUG
-            ApiUsername = "testUser";
+            ApiUsername = "username";
 #else
             ApiUsername = ConfigurationManager.AppSettings["ApiUser"];
 #endif 
 
 #if DEBUG
-            ApiPassword = "http://localhost:64196";
+            ApiPassword = "password";
 #else
             ApiPassword = ConfigurationManager.AppSettings["ApiPassword"];
 #endif
@@ -60,7 +60,7 @@ namespace JobEngine.Client
 #endif
 
 #if DEBUG
-            JobEngineClientId = new Guid("38AFF521-11FB-E411-827B-E8B1FC46C78C");
+            JobEngineClientId = new Guid("F120F9C0-5ACF-E511-828B-4C34883F5CC2");
 #else
             JobEngineClientId = Guid.Parse(ConfigurationManager.AppSettings["JobEngineClientId"]);
 #endif
---
+++ src/Projects/JobEngine.Core.Persistence/_CreateDatabase/seed.sql
@@ -0,0 +1,67 @@
+  INSERT INTO JobType
+  (Id, Name)
+  VALUES(1,'AssemblyJob')
+
+    INSERT INTO JobType
+  (Id, Name)
+  VALUES(2,'Powershell')
+
+  INSERT INTO LogLevel
+  (Id, Name)
+  VALUES (0, 'DEBUG')
+	
+  INSERT INTO LogLevel
+  (Id, Name)
+  VALUES (1, 'INFO')
+
+  INSERT INTO LogLevel
+  (Id, Name)
+  VALUES (2, 'WARN')
+
+  INSERT INTO LogLevel
+  (Id, Name)
+  VALUES (3, 'ERROR')
+
+  INSERT INTO LogLevel
+  (Id, Name)
+  VALUES (4, 'FATAL')
+
+  INSERT INTO PSResultType
+  ( Name)
+  VALUES ( 'Table')
+
+    INSERT INTO PSResultType
+  ( Name)
+  VALUES ( 'String')
+
+   INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (0, 'NOT_RECIEVED_BY_CLIENT')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (1, 'DOWNLOADED_BY_CLIENT')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (2, 'EXECUTING')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (3, 'SUCCESS')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (4, 'WARNING')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (5, 'FAILED')
+
+      INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (6, 'ERROR')
+
+        INSERT INTO JobExecutionStatus
+  ( Id, Name)
+  VALUES (7, 'FATAL')
